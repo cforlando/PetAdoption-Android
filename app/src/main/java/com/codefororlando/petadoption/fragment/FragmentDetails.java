2d5ce5e8b0d0e8cfab5b20bfb0095b2f01058428
JohnLeeroy <johnliglobal@gmail.com>
2016-05-22 15:27:56 -0400

    Updated the endpoint.  Added an import to get the project to build.

--- app/src/main/java/com/codefororlando/petadoption/fragment/FragmentDetails.java
+++ app/src/main/java/com/codefororlando/petadoption/fragment/FragmentDetails.java
@@ -12,6 +12,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.transition.AutoTransition;
