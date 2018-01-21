eb1ff7ba1ade1d6e2ed6d51d82d05464f7ed384f
John Li <johnliglobal@gmail.com>
2018-01-21 15:31:43 -0500

    Refactored HorizontalViewPagerIndicator constructor and converted pager adapter to kotlin

--- app/src/main/java/com/codefororlando/petadoption/recyclerview/HorizontalViewPagerIndicator.kt
+++ app/src/main/java/com/codefororlando/petadoption/recyclerview/HorizontalViewPagerIndicator.kt
@@ -12,30 +12,36 @@ import android.view.animation.ScaleAnimation
 import android.widget.LinearLayout
 import com.codefororlando.petadoption.R
 
-class HorizontalViewPagerIndicator : LinearLayout, ViewPager.OnPageChangeListener, ViewPager.OnAdapterChangeListener {
+class HorizontalViewPagerIndicator @JvmOverloads constructor(context: Context,
+                                                             attrs: AttributeSet? = null,
+                                                             defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr), ViewPager.OnPageChangeListener, ViewPager.OnAdapterChangeListener {
 
     private var pageCount = 0
     private var selectedIndex = 0
-    private var indicatorViews: MutableList<View> = mutableListOf()
-    private lateinit var scaleUpAnim: ScaleAnimation
-    private lateinit var scaleDownAnim: ScaleAnimation
+    private val indicatorViews: MutableList<View> = mutableListOf()
+    private val scaleUpAnim: ScaleAnimation
+    private val scaleDownAnim: ScaleAnimation
 
-    constructor(context: Context) : super(context) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init()
-    }
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init()
-    }
-
-    private fun init() {
+    init {
         orientation = LinearLayout.HORIZONTAL
         gravity = Gravity.CENTER
-        initAnimations()
+        scaleUpAnim = ScaleAnimation(DEFAULT_INDICATOR_SCALE,
+                LARGE_INDICATOR_SCALE,
+                DEFAULT_INDICATOR_SCALE,
+                LARGE_INDICATOR_SCALE,
+                Animation.RELATIVE_TO_SELF,
+                .5f,
+                Animation.RELATIVE_TO_SELF,
+                .5f)
+        scaleDownAnim = ScaleAnimation(LARGE_INDICATOR_SCALE,
+                DEFAULT_INDICATOR_SCALE,
+                LARGE_INDICATOR_SCALE,
+                DEFAULT_INDICATOR_SCALE,
+                Animation.RELATIVE_TO_SELF,
+                .5f,
+                Animation.RELATIVE_TO_SELF,
+                .5f)
+        configureAnimations()
     }
 
     fun bind(viewPager: ViewPager) {
@@ -65,26 +71,10 @@ class HorizontalViewPagerIndicator : LinearLayout, ViewPager.OnPageChangeListene
         return inflater.inflate(R.layout.circle_layout, this, false)
     }
 
-    private fun initAnimations() {
-        scaleUpAnim = ScaleAnimation(DEFAULT_INDICATOR_SCALE,
-                LARGE_INDICATOR_SCALE,
-                DEFAULT_INDICATOR_SCALE,
-                LARGE_INDICATOR_SCALE,
-                Animation.RELATIVE_TO_SELF,
-                .5f,
-                Animation.RELATIVE_TO_SELF,
-                .5f)
+    private fun configureAnimations() {
         scaleUpAnim.fillAfter = true
         scaleUpAnim.duration = 300
 
-        scaleDownAnim = ScaleAnimation(LARGE_INDICATOR_SCALE,
-                DEFAULT_INDICATOR_SCALE,
-                LARGE_INDICATOR_SCALE,
-                DEFAULT_INDICATOR_SCALE,
-                Animation.RELATIVE_TO_SELF,
-                .5f,
-                Animation.RELATIVE_TO_SELF,
-                .5f)
         scaleDownAnim.fillAfter = true
         scaleDownAnim.duration = 300
     }
---
+++ app/src/main/java/com/codefororlando/petadoption/view/PetImageViewPagerAdapter.kt
@@ -0,0 +1,58 @@
+package com.codefororlando.petadoption.view
+
+import android.content.Context
+import android.support.annotation.DrawableRes
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+import com.codefororlando.petadoption.R
+import com.squareup.picasso.Picasso
+
+internal class PetImageViewPagerAdapter(context: Context) : PagerAdapter() {
+
+    private var images: MutableList<String> = mutableListOf()
+    @DrawableRes private var placeholderImage: Int = 0
+    private val mLayoutInflater: LayoutInflater
+
+    init {
+        mLayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    }
+
+    fun setImages(imageList: List<String>, @DrawableRes placeholderImage: Int) {
+        images.clear()
+        images.addAll(imageList)
+        this.placeholderImage = placeholderImage
+        notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        return if (images == null) {
+            0
+        } else images!!.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val itemView = mLayoutInflater.inflate(R.layout.image_pager_item, container, false)
+        val imageView = itemView.findViewById(R.id.image_content) as ImageView
+        Picasso.with(container.context)
+                .load(images!![position])
+                .resize(1000, 1000)
+                .onlyScaleDown()
+                .centerCrop()
+                .placeholder(placeholderImage)
+                .into(imageView)
+        container.addView(itemView)
+        return itemView
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+}
